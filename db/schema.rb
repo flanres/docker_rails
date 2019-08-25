ef9a3c6eece769e3a804cddadebd437054542c8d
Kosuke <gekkouzann334@gmail.com>
2019-08-26 00:05:17 +0900

    schema commit

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,11 @@ ActiveRecord::Schema.define(version: 2019_08_19_115025) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.boolean "admin"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
+    t.boolean "admin", default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
